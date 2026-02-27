4e578730a2b39caf80a533fc782a01c5ed07f62f
vasavi <gorlavasavi123@gmail.com>
2026-02-27 16:04:54 +0530

    javascript done

---
+++ javascript/script.js
@@ -0,0 +1,65 @@
+// console.log("Hello World!");
+// // Reinitialize 
+// var a;//global scope
+// a=10;
+// var a;
+// a=20;
+// let b=10;//block scope
+// b=20;
+// // cant redeclare
+// const c=30;//block scope
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="vasavi";
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=123456789980n;
+// var symbol=Symbol('li');
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
+// var arr=[1,2,3,4];
+// console.log(typeof arr);
+// var Obj={
+//     name:"vasavi",
+//     dept:"DS"
+// }
+// console.log(Obj);
+// console.log(typeof Obj);
+
+// Arithmetic operators
+var a=10;
+var b="20";
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+console.log(a%b)
+// Relational operator
+var a=10;
+var b="20";
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a>=b)
+console.log(a<=b)
+
+// logical operator
+var a=true;
+var b=false;
+console.log(a&&b)
+console.log(a||b)
+console.log(!a)
+
+
+
